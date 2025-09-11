de30195dbb9937ac021c9fdfb10443d47585fdde
will <morrisonwill0101@gmail.com>
2025-09-10 17:08:58 -0700

    ADD: finder and writer scripts for assignment 1

---
+++ finder-app/finder.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# aesd assignment 1 writer.
+# Will Morrison
+
+filesDir=$1
+if [ -z "${filesDir}" ]; then
+    echo "File directory is missing!"
+    exit 1
+elif [ ! -d "${filesDir}" ]; then
+    echo "Specified file directory does not exist!"
+    exit 1
+fi
+    
+searchStr=$2
+if [ -z "${searchStr}" ]; then
+    echo "Search String is missing!"
+    exit 1
+fi
+
+fileCount=$(find "${filesDir}" -type f | wc -l)
+lineCount=$(grep -r "${searchStr}" ${filesDir} | wc -l)
+echo "The number of files are ${fileCount} and the number of matching lines are ${lineCount}"
---
+++ finder-app/writer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Verify that arguments are present
+writeFile=$1
+if [ -z "${writeFile}" ]; then
+    echo "Wirte file is missing!"
+    exit 1
+fi
+
+writeStr=$2
+if [ -z "${writeStr}" ]; then
+    echo "File content is missing!"
+    exit 1
+fi
+
+mkdir -p $(dirname ${writeFile})
+echo ${writeStr} > ${writeFile}
+
