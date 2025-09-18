1939109c5f679985abe26f9bec00e40f11fb3f79
will <morrisonwill0101@gmail.com>
2025-09-17 22:01:10 -0700

    Fix Makefile not working

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -15,16 +15,8 @@
 
 int main(int argc, char *arg[]) {
 
-    //setvbuf(stdout, NULL, _IOLBF, 0);
-    printf("Hello World\n");
-
     // Setup Log
     openlog("writer", LOG_CONS, LOG_USER);
-
-    printf("Opened log\n");
-    printf("argc: %d\n", argc);
-    printf("arg1: %s arg2: %s\n", arg[0], arg[1]);
-
 
     // Verify args
     if(argc != 3) {
@@ -40,6 +32,8 @@ int main(int argc, char *arg[]) {
         syslog(LOG_ERR, "Failed to create file!");
         return 1;
     }
+
+    syslog(LOG_DEBUG, "Writing %s to %s", arg[1], arg[2]);
 
     ssize_t nr;
     nr = write(fd, arg[2], sizeof(arg[2]));
